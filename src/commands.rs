c47225584aa4419ffca07ea7f6c8be215966fc23
Rafael Villegas <rafa.villegas.michel@gmail.com>
2024-02-20 22:31:13 -0500

    Improving error handling
    Some resusable functions and error types still need to be created, and a couple of unrwaps still need to be replaced.

--- src/commands.rs
+++ src/commands.rs
@@ -4,14 +4,22 @@ use crate::{config, files};
 
 pub fn add(file_path: &str) -> io::Result<()> {
     let original_file_path = PathBuf::from(file_path);
-    let file_name = original_file_path.file_name().unwrap(); // TODO: remove unwrap
+    let file_name = original_file_path
+        .file_name()
+        .ok_or_else(|| io::Error::new(io::ErrorKind::NotFound, "File name not found"))?;
+
     let destination_file_path = PathBuf::from(file_name);
 
     let config_path = PathBuf::from(config::CONFIG_FILE_NAME);
-    let config = config::load(&files::read(&config_path)?).unwrap(); // TODO: remove unwrap
+    let config = config::load(&files::read(&config_path)?)
+        .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
 
-    if config::has(&config, &destination_file_path.to_str().unwrap()) {
-        // TODO: remove unwrap above
+    if config::has(
+        &config,
+        file_name.to_str().ok_or_else(|| {
+            io::Error::new(io::ErrorKind::InvalidData, "File name is not valid UTF-8")
+        })?,
+    ) {
         println!("File already being tracked");
         return Ok(());
     }
@@ -25,7 +33,9 @@ pub fn add(file_path: &str) -> io::Result<()> {
         link.to.to_str().unwrap().to_owned(),   // TODO: remove unwrap, also add full aboslute path
     );
 
-    let config_buffer = config::save(&config).unwrap(); // TODO: remove unwrap
+    let config_buffer =
+        config::save(&config).map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+
     files::write(&config_path, &config_buffer)?;
 
     Ok(())
