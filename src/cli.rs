b50724f03e752f436e5355663a9734d1fab38225
Rafael Villegas Michel <rafa.villegas.michel@gmail.com>
2024-03-12 22:04:32 -0500

    Create a new dot.toml if there isn't one already there (#6)

--- src/cli.rs
+++ src/cli.rs
@@ -14,20 +14,21 @@ struct Cli {
 
 #[derive(Subcommand)]
 enum Command {
+    /// Configures the current directory to be able to track new files
+    Init,
     /// Tracks a new file/directory by moving it to the current directory and creating a symlink to its
     /// original location
     Add {
         /// Path to the file/directory to track
         path: String,
     },
-    /// Removes a file from the dot_config.toml, deletes the symlink and returns it to the original
+    /// Removes a file from the manifest, deletes the symlink and returns it to the original
     /// location
     Remove {
         /// Path to the file/directory to remove
         path: String,
     },
-    /// Goes through the dot_config.toml and creates symlinks for each file if there isn't already
-    /// one
+    /// Goes through the manifest and creates symlinks for each file
     Sync,
 }
 
@@ -35,6 +36,7 @@ pub fn parse() -> io::Result<()> {
     let cli = Cli::parse();
 
     match &cli.command {
+        Command::Init => handlers::init(),
         Command::Add { path: file_path } => handlers::add(&file_path),
         Command::Remove { path: file_path } => handlers::remove(&file_path),
         Command::Sync => handlers::sync(),
