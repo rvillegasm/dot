b4bd7bc50fdbe6d439896eacca6eb32ac471141b
Rafael Villegas <rafa.villegas.michel@gmail.com>
2025-07-01 11:58:57 -0500

    Fix lint issue

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ mod service;
 
 fn main() {
     if let Err(e) = cli::parse() {
-        eprintln!("\x1b[31mError: {}\x1b[0m", e);
+        eprintln!("\x1b[31mError: {e}\x1b[0m");
         std::process::exit(1);
     }
 }
